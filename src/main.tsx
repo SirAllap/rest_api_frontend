104f60d629c59414957b845b276a3683be6f158e
David PR <serallapdivad@gmail.com>
2024-01-19 16:20:02 +0000

    Imported header and footed as a layout between the app

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Header } from './components/header/Header.tsx'
+import Footer from './components/footer/Footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
+		<Header />
 		<App />
+		<Footer />
 	</React.StrictMode>
 )
