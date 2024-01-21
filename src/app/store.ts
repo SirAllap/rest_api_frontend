dbe7b050f2dc772e9c0165e99b3252469db5ce1b
David PR <serallapdivad@gmail.com>
2024-01-21 15:18:59 +0000

    added book slice to the store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import bookSlice from '../features/books/bookSlice'
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		books: bookSlice,
+	},
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
