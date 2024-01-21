2818fd53ac09dd5b0e61be83e6d2ee6961086775
David PR <serallapdivad@gmail.com>
2024-01-21 13:57:33 +0000

    moved the header and footer to be render on the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Book from './components/book/Book'
+import Footer from './components/footer/Footer'
+import { Header } from './components/header/Header'
 
 function App() {
 	return (
 		<>
+			<Header />
 			<Book />
+			<Footer />
 		</>
 	)
 }
