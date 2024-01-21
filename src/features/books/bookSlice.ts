97cb305d6d38656e93d115e3c17eff8ca9d8cb52
David PR <serallapdivad@gmail.com>
2024-01-21 17:32:08 +0000

    created the slice and thunk to create a new book

--- src/features/books/bookSlice.ts
+++ src/features/books/bookSlice.ts
@@ -1,17 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { IBook } from '../interfaces'
-import { fetchAllBooks } from './bookThunk'
+import { addNewBook, fetchAllBooks } from './bookThunk'
 import { RootState } from '../../app/store'
 
 interface BookState {
 	books: IBook[]
 	status: 'idle' | 'pending' | 'rejected' | 'fulfilled'
+	creationStatus: 'idle' | 'pending' | 'rejected' | 'fulfilled'
 	error: string | null
 }
 
 const initialState: BookState = {
 	books: [],
 	status: 'idle',
+	creationStatus: 'idle',
 	error: 'null',
 }
 
@@ -31,6 +33,16 @@ const bookSlice = createSlice({
 				state.books = action.payload
 				state.status = 'fulfilled'
 			})
+
+			.addCase(addNewBook.pending, (state) => {
+				state.creationStatus = 'pending'
+			})
+			.addCase(addNewBook.rejected, (state) => {
+				state.creationStatus = 'rejected'
+			})
+			.addCase(addNewBook.fulfilled, (state) => {
+				state.creationStatus = 'fulfilled'
+			})
 	},
 })
 
@@ -38,3 +50,5 @@ export default bookSlice.reducer
 
 export const fetchBooks = (state: RootState) => state.books.books
 export const fetchStatus = (state: RootState) => state.books.status
+export const fetchCreationStatus = (state: RootState) =>
+	state.books.creationStatus
--- src/features/books/bookThunk.ts
+++ src/features/books/bookThunk.ts
@@ -6,7 +6,33 @@ const API_URL = import.meta.env.VITE_API_URL
 export const fetchAllBooks = createAsyncThunk<IBook[]>(
 	'books/fetchAllBooks',
 	async () => {
-		const response = await fetch(`${API_URL}/books`)
+		const response = await fetch(`${API_URL}/books`, {
+			method: 'GET',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+		if (!response.ok) {
+			throw new Error(`Response status: ${response.status}`)
+		} else {
+			const data = await response.json()
+			return data
+		}
+	}
+)
+
+export const addNewBook = createAsyncThunk(
+	'books/addNewBook',
+	async (body: IBook) => {
+		const response = await fetch(`${API_URL}/books`, {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(body),
+		})
 		if (!response.ok) {
 			throw new Error(`Response status: ${response.status}`)
 		} else {
